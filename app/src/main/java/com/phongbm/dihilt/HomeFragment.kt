c2e2f64d480d7bc8d454e668951780c5889531c3
PhongBM <phongbm.it@gmail.com>
2020-06-17 23:00:02 +0700

    Project: init

---
+++ app/src/main/java/com/phongbm/dihilt/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.phongbm.dihilt
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
